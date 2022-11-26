05ef227f8674d7bc626ab549230a7a246c681349
MastroJolly <jolaosoyusuf@gmail.com>
2022-11-26 22:08:38 +0100

    added the endpoint to get single book

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Author struct {
 }
 
 type Book struct {
-	ID     int64   `json:"id"`
+	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
@@ -31,13 +31,26 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+func getBook(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	w.Header().Set("Content-Type", "application/json")
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
 func main() {
 
-	books = append(books, Book{ID: 1, Title: "Title 1", Author: &Author{Title: "Mr", Name: "John Doe"}})
-	books = append(books, Book{ID: 2, Title: "Title 2", Author: &Author{Title: "Mrs", Name: "Jane Smith"}})
+	books = append(books, Book{ID: "123", Title: "Title 1", Author: &Author{Title: "Mr", Name: "John Doe"}})
+	books = append(books, Book{ID: "456", Title: "Title 2", Author: &Author{Title: "Mrs", Name: "Jane Smith"}})
 
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/api/test", printHelloWorld)
 	r.HandleFunc("/api/books", getBooks)
+	r.HandleFunc("/api/books/{id}", getBook)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
