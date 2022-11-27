7a906880e9135373b534cc934368c9caf9981bc0
MastroJolly <jolaosoyusuf@gmail.com>
2022-11-27 12:22:29 +0100

    created the create endpoint

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -43,14 +45,37 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Book{})
 }
 
+func createBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(books)
+}
+
+// func deleteBook(w http.ResponseWriter, r *http.Request) {
+// 	id := mux.Vars(r)["id"]
+// 	w.Header().Set("Content-Type", "application/json")
+// 	deletedBooks := []Book{}
+// 	for _, book := range books {
+// 		if book.ID != id {
+// 			deletedBooks = append(deletedBooks, book)
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(deletedBooks)
+// }
+
 func main() {
 
 	books = append(books, Book{ID: "123", Title: "Title 1", Author: &Author{Title: "Mr", Name: "John Doe"}})
 	books = append(books, Book{ID: "456", Title: "Title 2", Author: &Author{Title: "Mrs", Name: "Jane Smith"}})
 
 	r := mux.NewRouter().StrictSlash(true)
-	r.HandleFunc("/api/test", printHelloWorld)
-	r.HandleFunc("/api/books", getBooks)
-	r.HandleFunc("/api/books/{id}", getBook)
+	r.HandleFunc("/api/test", printHelloWorld).Methods("GET")
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBooks).Methods("POST")
+	// r.HandleFunc("/api/books/{id}", deleteBook)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
